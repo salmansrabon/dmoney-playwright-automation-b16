6a0ffc9a6b96c975898b3726d81fe9203ca6c54a
salmansrabon <salmansrabon@gail.com>
2025-10-30 23:31:59 +0600

    Refactor Gmail service functions and update tests to read latest message

--- services/gmail.service.ts
+++ services/gmail.service.ts
@@ -15,5 +15,21 @@ export async function fetchGmailMessages(
 
   if (!res.ok()) throw new Error(`Gmail API error ${res.status()}`);
 
-  return res.json();
+  const response = await res.json();
+  return response.messages[0].id;
+}
+export async function readLatestMessage(request: APIRequestContext, accessToken: string | undefined) {
+  if (!accessToken) throw new Error("Missing Gmail access token");
+  const messageId = await fetchGmailMessages(request, accessToken);
+  const res = await request.get(
+    `https://gmail.googleapis.com/gmail/v1/users/me/messages/${messageId}`,
+    {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    }
+  );
+
+  if (!res.ok()) throw new Error(`Gmail API error ${res.status()}`);
+
+  const jsonResponse = await res.json();
+  return jsonResponse.snippet;
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { loginRequest } from "../services/login.service";
-import { fetchGmailMessages } from "../services/gmail.service.ts";
+import { fetchGmailMessages, readLatestMessage } from "../services/gmail.service.ts";
 import { saveEnvVar } from "../utils/utils";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -16,12 +16,10 @@ test("Admin can login via API and save token to .env", async ({ request }) => {
   saveEnvVar("TOKEN", response.token, ".env");
 });
 
-test.only("Load gmail list", async ({ request }) => {
-    //console.log(process.env.GMAIL_ACCESS_TOKEN)
-  const res = await fetchGmailMessages(
+test("Read latest Gmail message", async ({ request }) => {
+  const res = await readLatestMessage(
     request,
     process.env.GMAIL_ACCESS_TOKEN
   );
-  const messageId=res.messages[0].id;
-  saveEnvVar("GMAIL_MESSAGE_ID", messageId, ".env");
+    console.log(res);
 });
